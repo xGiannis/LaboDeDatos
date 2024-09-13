b8fef61d8b8efdac838911f9fc67c7928ba832ba
Gian Lucca Sanza <gianluccalord723@gmail.com>
2024-09-13 01:06:59 -0300

    casi termino la seccion RESUMEN

--- guia_practica_SQL/Guía Práctica - SQL - Archivos adjuntos-20240910/resolucion.py
+++ guia_practica_SQL/Guía Práctica - SQL - Archivos adjuntos-20240910/resolucion.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
 Created on Tue Sep 10 14:31:17 2024
 
@@ -15,6 +14,7 @@ Fecha  : 2024-03-25
 #%%===========================================================================
 # Importamos bibliotecas
 import pandas as pd
+import numpy as np
 from inline_sql import sql, sql_val
 
 
@@ -22,7 +22,7 @@ from inline_sql import sql, sql_val
 # Importamos los datasets que vamos a utilizar en este programa
 #=============================================================================
 
-carpeta = "~/Escritorio/guia_practica_SQL/Guía Práctica - SQL - Archivos adjuntos-20240910/"
+carpeta = "C:/Users/usuario/Desktop/LaboDeDatos/guia_practica_SQL/Guía Práctica - SQL - Archivos adjuntos-20240910/"
 
 
 # Ejercicios AR-PROJECT, SELECT, RENAME
@@ -222,24 +222,314 @@ dataframeResultado = sql^ consultaSQL
 """a. Devolver un listado con los nombres de los departamentos que no tienen
 ningún caso asociado."""
 
-depas= sql^"""
-SELECT d.id
-FROM departamento as d
-INNER JOIN provincia AS p
-ON d.id_provincia = p.id
+consultaSQL = """
+SELECT DISTINCT d.descripcion
+FROM departamento AS d
+LEFT OUTER JOIN casos AS c
+ON d.id = c.id_depto
+WHERE c.cantidad = NULL
               """
 
+dataframeResultado = sql^ consultaSQL
+#todos los casos estan asociados, perdi tiempo de mi vida con esta pija
+
+"""b. Devolver un listado con los tipos de evento que no tienen ningún caso
+asociado.
+"""
 consultaSQL = """
-SELECT *
-FROM casos as c
-INNER JOIN depas
-ON c.id_depto = depas.id
-WHERE c.cantidad = 0
+SELECT DISTINCT descripcion
+FROM tipoevento AS t
+LEFT OUTER JOIN casos AS c
+ON t.id = c.id_tipoevento
+WHERE anio IS NULL
               """
 
 
 dataframeResultado = sql^ consultaSQL
 
 
+#D. Consultas resumen
 
-    
+"""a)Calcular la cantidad total de casos que hay en la tabla casos.
+"""
+
+
+consultaSQL = """
+SELECT SUM(cantidad) as CASOSTOTALES
+FROM casos
+              """
+
+dataframeResultado = sql^ consultaSQL
+
+
+
+"""b. Calcular la cantidad total de casos que hay en la tabla casos para cada año y
+cada tipo de caso. Presentar la información de la siguiente manera:
+descripción del tipo de caso, año y cantidad. Ordenarlo por tipo de caso
+(ascendente) y año (ascendente).
+"""
+
+
+consultaSQL = """
+SELECT SUM(c.cantidad) as CASOSTOTALES, c.anio,c.id_tipoevento,t.descripcion
+FROM casos as c
+INNER JOIN tipoevento as t
+ON c.id_tipoevento = t.id
+GROUP BY c.anio , c.id_tipoevento, t.descripcion
+ORDER BY id_tipoevento ASC, anio ASC
+
+              """
+
+dataframeResultado = sql^ consultaSQL
+
+
+"""c. Misma consulta que el ítem anterior, pero sólo para el año 2019.
+
+"""
+
+
+consultaSQL = """
+SELECT SUM(c.cantidad) as CASOSTOTALES, c.anio,c.id_tipoevento,t.descripcion
+FROM casos as c
+INNER JOIN tipoevento as t
+ON c.id_tipoevento = t.id
+WHERE c.anio = 2019
+GROUP BY c.anio , c.id_tipoevento, t.descripcion
+ORDER BY id_tipoevento ASC, anio ASC
+              """
+
+dataframeResultado = sql^ consultaSQL
+
+
+"""d. Calcular la cantidad total de departamentos que hay por provincia. Presentar
+la información ordenada por código de provincia.
+"""
+
+
+depasTotalesPorProvincia = sql ^ """
+SELECT COUNT(*) as depasPorProvincia, id_provincia, p.descripcion
+FROM departamento
+INNER JOIN provincia as p
+ON id_provincia = p.id
+GROUP BY id_provincia, p.descripcion
+
+
+"""
+
+#consultaSQL = 
+"""
+SELECT p.id
+FROM provincia as p
+INNER JOIN tipoevento as t
+ON c.id_tipoevento = t.id
+GROUP BY p.id
+ORDER BY p.id
+              """
+
+dataframeResultado = sql^ consultaSQL
+
+
+
+
+
+
+"""e. Listar los departamentos con menos cantidad de casos en el año 2019.
+
+"""
+
+
+consultaSQL = """
+SELECT DISTINCT d.descripcion, SUM(c.cantidad) as casosPorDepa, d.id
+FROM departamento as d
+INNER JOIN casos as c
+ON c.id_depto = d.id
+WHERE c.anio = 2019 
+GROUP BY d.descripcion, d.id
+ORDER BY casosPorDepa ASC
+LIMIT 10
+              """
+              
+
+dataframeResultado = sql^ consultaSQL
+
+
+"""f. Listar los departamentos con más cantidad de casos en el año 2020.
+"""
+
+
+
+consultaSQL = """
+SELECT d.descripcion, SUM(c.cantidad) as casosPorDepa, d.id,d.id_provincia
+FROM departamento as d
+INNER JOIN casos as c
+ON c.id_depto = d.id
+WHERE c.anio = 2020
+GROUP BY  d.id, d.id_provincia, d.descripcion
+ORDER BY casosPorDepa DESC
+LIMIT 15
+              """
+
+#los resultados son logicos (no racism). Es dengue, no covid, tiene sentido q el norte este foerte
+
+dataframeResultado = sql^ consultaSQL
+
+
+"""g. Listar el promedio de cantidad de casos por provincia y año.
+
+"""
+
+casosAño = consultaSQL = sql^"""
+SELECT DISTINCT d.descripcion, SUM(c.cantidad) as casosPorDepa, d.id,d.id_provincia,c.anio
+FROM departamento as d
+INNER JOIN casos as c
+ON c.id_depto = d.id
+GROUP BY  d.id, d.id_provincia, d.descripcion,c.anio
+ORDER BY c.anio DESC,casosPorDepa DESC
+              """
+
+
+
+
+consultaSQL = """
+SELECT AVG(c.casosPorDepa) as promedio_anio, c.anio, p.descripcion
+FROM casosAño as c
+INNER JOIN provincia as p
+ON c.id_provincia = p.id
+GROUP BY c.anio, p.descripcion 
+ORDER BY c.anio DESC
+
+              """
+
+
+dataframeResultado = sql^ consultaSQL
+
+"""h. Listar, para cada provincia y año, cuáles fueron los departamentos que más
+cantidad de casos tuvieron"""
+
+#podria hacer un limit 24 pero seria medio de cagon
+
+maxCasosPorProvincia = sql^"""
+SELECT MAX(casosPorDepa) as casosMax, id_provincia, anio, p.descripcion as provincia
+FROM casosAño
+INNER JOIN provincia as p
+ON p.id = id_provincia
+GROUP BY p.descripcion, anio,id_provincia
+
+              """
+              
+casosPorProvincia = sql^"""
+SELECT casosPorDepa as casos, id_provincia, anio, p.descripcion as provincia, casosAño.id, casosAño.descripcion as depa
+FROM casosAño
+INNER JOIN provincia as p
+ON p.id = id_provincia
+GROUP BY p.descripcion, anio,id_provincia, casosPorDepa, casosAño.id, depa
+"""
+
+
+
+consultaSQL = """
+SELECT DISTINCT casosMax, mp.provincia, mp.anio, depa
+FROM casosPorProvincia as cp
+INNER JOIN maxCasosPorProvincia as mp
+ON cp.id_provincia = mp.id_provincia AND casosMax = casos AND cp.anio = mp.anio 
+GROUP BY mp.provincia, mp.anio, casosMax, depa
+ORDER BY mp.anio DESC
+              """
+
+         
+              
+#yendo a dormir sin haberlo resuelto
+
+dataframeResultado = sql^ consultaSQL
+
+"""i. Mostrar la cantidad de casos total, máxima, mínima y promedio que tuvo la
+provincia de Buenos Aires en el año 2019.
+"""
+
+casosBsas = sql^"""
+SELECT cantidad, id_depto, anio, id_provincia
+FROM casos
+INNER JOIN departamento
+ON id_depto = departamento.id
+WHERE anio = 2019 and id_provincia = 6
+
+"""
+
+
+#####asumi q se el id, si no es hacer un inner join con prov y meterle q la descripcion sea buenos aires
+
+
+########
+consultaSQL = """
+SELECT DISTINCT SUM(cantidad),MAX(cantidad), MIN(cantidad), AVG(cantidad)
+FROM casosBsas
+              """
+
+dataframeResultado = sql^ consultaSQL
+
+
+
+"""j. Misma consulta que el ítem anterior, pero sólo para aquellos casos en que la
+cantidad total es mayor a 1000 casos.
+"""
+
+depasPorProv=sql^"""
+SELECT DISTINCT d.id, d.descripcion as depa, d.id_provincia, p.descripcion as prov, (cantidad), anio
+FROM departamento as d
+INNER JOIN provincia as p
+ON p.id = id_provincia
+INNER JOIN casos as c
+ON c.id_depto=d.id
+ORDER BY id_provincia
+
+"""
+
+
+
+casosPorProv1000=sql^"""
+SELECT SUM(cantidad) as cantProv, id_provincia, prov
+FROM depasPorProv
+GROUP BY id_provincia, prov
+HAVING cantProv > 1000
+
+"""
+
+
+
+consultaSQL = """
+SELECT DISTINCT SUM(cantidad),MAX(cantidad), MIN(cantidad), AVG(cantidad), prov
+FROM depasPorProv
+GROUP BY id_provincia, prov
+HAVING SUM(cantidad) >(
+       SELECT SUM(cantidad) as cantProv
+       FROM depasPorProv
+       GROUP BY id_provincia
+       HAVING cantProv > 1000
+)
+              """
+              
+              #LE FALATAN CORDOBA Y CORRIENTES
+              #WTF, CADA VEZ Q LO CORRO DA ALGO DIFERENTE
+              #preguntar!!!!!!
+              #puede ser q sea porque no especifico año
+
+ALGODIF = sql^ consultaSQL
+
+
+consultaSQL2 =sql^"""
+SELECT DISTINCT SUM(dp.cantidad),MAX(dp.cantidad), MIN(dp.cantidad), AVG(dp.cantidad), d.prov
+FROM casosPorProv1000 as d
+INNER JOIN depasPorProv as dp
+ON d.prov = dp.prov
+WHERE dp.anio = 2019
+GROUP BY d.prov
+
+"""
+
+#esta creo q da bien
+
+
+
+
+
+
